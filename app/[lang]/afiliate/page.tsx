edff8efc24b10e1edf446be3e497fbd238884f47
Tech Rebel <albchamo@gmail.com>
2026-09-11 23:03:11 -0600

    fix: localize membership form validation

--- app/[lang]/afiliate/page.tsx
+++ app/[lang]/afiliate/page.tsx
@@ -64,7 +64,7 @@ export default async function ApplicationPage({
             </ol>
           </aside>
           <div>
-            <MembershipApplicationForm copy={a} />
+            <MembershipApplicationForm copy={a} locale={lang} />
           </div>
         </div>
       </section>
